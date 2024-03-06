3ee174f32f966140e1266aefc3ec822ff6f77c2d
FadriPestalozzi <fadri.pestalozzi@gmail.com>
2024-03-06 15:28:06 +0100

    toggle button run/stop

---
+++ cs_wpf_tutorial/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cs_wpf_tutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                //stop
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                //run
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+
+            running = !running; //flip boolean
+        }
+    }
+}
